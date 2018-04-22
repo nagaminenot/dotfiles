5a3dc72eb22279692c579f467762c7c364241301
nagaminenot <nagamine@efg.company>
2018-04-22 16:46:27 +0900

    ステータスバー表示

--- xmonad.hs
+++ xmonad.hs
@@ -1,11 +1,18 @@
 import XMonad
+import XMonad.Hooks.DynamicLog
 
+-- 起動時のコード
 myStartupHook   = do
     spawn "redshift"
     spawn "fcitx-autostart"
     spawn "nitrogen --restore"
 
-main = xmonad defaultConfig
-     { 
-         startupHook    = myStartupHook
-     }
+-- 設定
+myConfig = defaultConfig {
+    startupHook    = myStartupHook
+    -- デバッグ用
+    -- , focusedBorderColor = "#0000ff"
+}
+
+-- 実行
+main = xmonad =<< xmobar myConfig
