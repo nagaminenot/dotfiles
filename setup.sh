091d90596b58fef269c46c698d3edbfefe5b5e98
nagaminenot <nagamine@efg.company>
2018-04-28 12:21:55 +0900

    ドットファイルの名前変更・シンボリックリンク設置コマンド作成

---
+++ setup.sh
@@ -0,0 +1,25 @@
+
+#!/bin/bash
+
+# 連想配列の宣言
+declare -A FILES;
+# ファイル名:パス名
+FILES=(
+  ["dot.bashrc"]="${HOME}/.bashrc"
+  ["dot.bash_profile"]="${HOME}/.bash_profile"
+  ["dot.xprofile"]="${HOME}/.xprofile"
+  ["dot.xinitrc"]="${HOME}/.xinitrc"
+  ["dot.Xresources"]="${HOME}/.Xresources"
+  ["dot.xmobarrc"]="${HOME}/.xmobarrc"
+  ["config.fish"]="${HOME}/.config/fish/config.fish"
+  ["xmonad.hs"]="${HOME}/.xmonad/xmonad.hs"
+  ["terminator_config"]="${HOME}/.config/terminator/config"
+)
+# 連想配列のループ
+for FILE in ${!FILES[@]};
+do
+    echo ${FILES[$FILE]}
+    ln -nfsv $HOME/dotfiles/$FILE ${FILES[$FILE]}
+done
+
+exit 0
