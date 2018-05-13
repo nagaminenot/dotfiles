04c2b13fab8c62f00575ca3de0dbcbeb36a61ac4
nagaminenot <nagamine@efg.company>
2018-05-14 02:10:47 +0900

    外部ディスプレイの解像度を100%に変更

--- display_settings/1v1.sh
+++ display_settings/1v1.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output HDMI-2 --off --output HDMI-1 --primary --mode 1920x1080 --pos 296x0 --rotate normal --output DP-1 --off --output eDP-1 --mode 2560x1440 --pos 0x1080 --rotate normal --output DP-2 --off
+xrandr --output HDMI-2 --off --output HDMI-1 --primary --mode 1920x1080 --scale 1.44x1.44 --pos 0x0 --rotate normal --output DP-1 --off --output eDP-1 --mode 2560x1440 --pos 104x1556 --rotate normal --output DP-2 --off
