9e222f6980b957efd994a6692ca706389e8e1b6d
nagaminenot <nagamine@efg.company>
2018-06-04 23:50:22 +0900

    インテルの内蔵グラボに合わせて外部ディスプレイの設定調整

--- display_settings/1v1.sh
+++ display_settings/1v1.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output HDMI-2 --off --output HDMI-1 --primary --mode 1920x1080 --scale 1.44x1.44 --pos 0x0 --rotate normal --output DP-1 --off --output eDP-1 --mode 2560x1440 --pos 104x1556 --rotate normal --output DP-2 --off
+xrandr --output VIRTUAL1 --off --output eDP1 --mode 2560x1440 --pos 360x1556 --rotate normal --output DP1 --off --output HDMI2 --off --output HDMI1 --primary --mode 1920x1080 --scale 1.44x1.44  --pos 256x0 --rotate normal --output DP2 --off
